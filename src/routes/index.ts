d28e3b3622132f4555a2f7a87d5b19f5b7b99664
Léo Godard <leo@legodard.fr>
2023-03-23 20:38:43 -0400

    Finish UI

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,9 +4,19 @@ import { createHash } from 'crypto';
 
 var router = Router();
 
+interface Message {
+  message: string;
+  isBot: boolean;
+}
+
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  const messages: Message[] = [
+    { message: 'Your name is Blahblah', isBot: false },
+    { message: 'Hi my name is Blahblah. How can I help you ?', isBot: true }
+  ];
   res.render('index', {
     userProfile: {...req.oidc.user, gravatar: createHash('md5').update(req.oidc?.user?.email).digest('hex')},
+    messages
   });
 });
 
