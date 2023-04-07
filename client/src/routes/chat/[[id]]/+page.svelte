b12faa6e61963dbd6491a3628ac9fda44bcabdf9
Léo Godard <leo@legodard.fr>
2023-04-07 19:02:07 -0400

    fix(client): fix overflow message

--- client/src/routes/chat/[[id]]/+page.svelte
+++ client/src/routes/chat/[[id]]/+page.svelte
@@ -209,7 +209,7 @@
           <div class="flex">
             <img src="{message.isBot ? '/robot.svg' : '/person.svg'}" alt="{message.isBot ? 'Robot' : 'User'}" class="me-2 rounded-sm w-[50px] h-[50px]">
           </div>
-          <p class="flex-1 mb-0 ml-1 dark:text-white text-black">
+          <p class="flex-1 mb-0 ml-1 dark:text-white text-black w-[95%]">
             {@html Markdown(message.message)}
           </p>
         </div>
