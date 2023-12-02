536fb0816f2029b137204682a8ef0f7824b93361
Léo Godard <leo@legodard.fr>
2023-12-02 14:53:33 +0100

    fix(client): cropped image depending viewport

--- client/src/components/chat/message.svelte
+++ client/src/components/chat/message.svelte
@@ -10,10 +10,10 @@
 </script>
 
 <div class="flex mb-2 flex-row">
-  <div class="flex">
-    <img src="{message.role === 'assistant' ? '/robot.svg' : '/person.svg'}" alt="{message.role === 'assistant' ? 'Robot' : 'User'}" class="me-2 rounded-sm w-[50px] h-[50px]">
+  <div class="flex w-12 h-12 me-1">
+    <img src="{message.role === 'assistant' ? '/robot.svg' : '/person.svg'}" alt="{message.role === 'assistant' ? 'Robot' : 'User'}" class="rounded-sm w-full h-full object-cover">
   </div>
-  <p class="flex-1 mb-0 ml-1 dark:text-white text-black w-[95%]">
+  <p class="flex-1 mb-0 ml-1 dark:text-white text-black w-[75%]">
     {#each tokens as token}
       {#if token.type === "code"}
         <Codeblock code={token.text} language={token.lang} />
