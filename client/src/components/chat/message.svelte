4e6352ced9f6078f5496ebdb17e39a7aa11921fc
Léo Godard <leo@legodard.fr>
2023-12-02 02:13:45 +0100

    rework(client): add beautiful codeblock

--- client/src/components/chat/message.svelte
+++ client/src/components/chat/message.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-  import Markdown from 'markdown-hljs';
+  import * as marked from 'marked';
+  import Codeblock from './codeblock.svelte';
 
   export let message: {
     message: string;
     role: string;
   };
+  $: tokens = marked.lexer(message.message);
 </script>
 
 <div class="flex mb-2 flex-row">
@@ -12,6 +14,12 @@
     <img src="{message.role === 'assistant' ? '/robot.svg' : '/person.svg'}" alt="{message.role === 'assistant' ? 'Robot' : 'User'}" class="me-2 rounded-sm w-[50px] h-[50px]">
   </div>
   <p class="flex-1 mb-0 ml-1 dark:text-white text-black w-[95%]">
-    {@html Markdown(message.message)}
+    {#each tokens as token}
+      {#if token.type === "code"}
+        <Codeblock code={token.text} />
+      {:else}
+        {@html marked.parse(token.raw)}
+      {/if}
+    {/each}
   </p>
 </div>
