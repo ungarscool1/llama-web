674dd4f6a313b024e0c2b87c64f730f83811a093
Léo Godard <leo@legodard.fr>
2023-12-04 02:29:16 +0100

    change send btn color in dark theme

--- client/src/routes/chat/[[id]]/+page.svelte
+++ client/src/routes/chat/[[id]]/+page.svelte
@@ -6,6 +6,7 @@
   import Sidebar from '../../../components/chat/sidebar.svelte';
   import Message from '../../../components/chat/message.svelte';
   import Welcome from '../../../components/chat/welcome.svelte';
+  import GoDown from '../../../components/chat/goDown.svelte';
 
   $: activeUrl = $page.url.pathname;
   $: chats = undefined;
@@ -170,9 +171,11 @@
       <div class="flex flex-col w-full flex-grow relative">
         <textarea disabled={isRequesting} draggable="false" rows="1" class="m-0 w-full resize-none border-1 border-solid rounded-md py-[10px] pr-10 pl-3 md:py-3.5 md:pr-12 md:pl-4 bg-transparent focus:ring-0 focus-visible:ring-0 dark:bg-transparent dark:text-white {isPromptError ? 'border-red-700' : ''}" style="max-height: 200px;" placeholder="Enter some text..." bind:value={prompt}></textarea>
         {#if !isRequesting}
-          <button class="absolute md:right-3 md:bottom-[0.6875rem] right-2 bottom-[0.4375rem] bg-blue-500 hover:bg-blue-600 disabled:opacity-10 text-white font-bold py-2 px-2 rounded-md" on:click|preventDefault={sendRequest} disabled={prompt.length === 0}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"/>
-          </svg></button>
+          <button class="absolute md:right-3 md:bottom-[0.6875rem] right-2 bottom-[0.4375rem] bg-blue-500 hover:bg-blue-600 dark:hover:bg-gray-900 dark:bg-white disabled:opacity-10 text-white font-bold py-2 px-2 rounded-md" on:click|preventDefault={sendRequest} disabled={prompt.length === 0}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up dark:text-black" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"/>
+            </svg>
+          </button>
         {:else}
         <button class="absolute md:right-3 md:bottom-[0.6875rem] right-2 bottom-[0.4375rem] bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-2 rounded-md" on:click|preventDefault={stopChat}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stop-circle" viewBox="0 0 16 16">
           <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
