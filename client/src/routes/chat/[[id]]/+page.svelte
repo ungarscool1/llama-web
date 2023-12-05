1bc186df9888bc93c5f7f033629f2ffea89d8bee
Léo Godard <leo@legodard.fr>
2023-12-05 02:20:09 +0100

    rework(client/chat): change stop generation button color

--- client/src/routes/chat/[[id]]/+page.svelte
+++ client/src/routes/chat/[[id]]/+page.svelte
@@ -13,7 +13,7 @@
   $: onChange(activeUrl);
   $: prompt = "";
   $: model = "";
-  let isRequesting = false;
+  let isRequesting = true;
   let isPromptError = false;
   let isError = false;
   let chatBox: HTMLDivElement;
@@ -196,9 +196,8 @@
             </svg>
           </button>
         {:else}
-        <button class="absolute md:right-3 md:bottom-[0.6875rem] right-2 bottom-[0.4375rem] bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-2 rounded-md" on:click|preventDefault={stopChat}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stop-circle" viewBox="0 0 16 16">
-          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-          <path d="M5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5z"/>
+        <button class="absolute md:right-3 md:bottom-[1rem] right-2 bottom-[0.75rem] text-white font-bold rounded-full border-2 border-gray-900 p-1 dark:border-gray-200" on:click|preventDefault={stopChat}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="h-2 w-2 text-gray-900 dark:text-gray-200" viewBox="0 0 16 16">
+          <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2z" stroke-width="0" />
         </svg></button>
         {/if}
       </div>
