25d8707e28adacbfd5031b7361d1f1eca201468b
Léo Godard <leo@legodard.fr>
2023-12-05 17:52:51 +0100

    fix(client/chat): fix stop button too low on mobile

--- client/src/routes/chat/[[id]]/+page.svelte
+++ client/src/routes/chat/[[id]]/+page.svelte
@@ -201,7 +201,7 @@
             </svg>
           </button>
         {:else}
-        <button class="absolute md:right-3 md:bottom-[1rem] right-2 bottom-[0.75rem] text-white font-bold rounded-full border-2 border-gray-900 p-1 dark:border-gray-200" on:click|preventDefault={stopChat}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="h-2 w-2 text-gray-900 dark:text-gray-200" viewBox="0 0 16 16">
+        <button class="absolute md:right-3 md:bottom-[1rem] right-2 bottom-[0.845rem] text-white font-bold rounded-full border-2 border-gray-900 p-1 dark:border-gray-200" on:click|preventDefault={stopChat}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="h-2 w-2 text-gray-900 dark:text-gray-200" viewBox="0 0 16 16">
           <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2z" stroke-width="0" />
         </svg></button>
         {/if}
