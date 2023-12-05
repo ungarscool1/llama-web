56a2580d93b1e0b68b53b440656b108b5b9d613e
Léo Godard <leo@legodard.fr>
2023-12-05 21:08:11 +0100

    fix(client/chat): interpret markdown in list

--- client/src/components/chat/messageComponent/list.svelte
+++ client/src/components/chat/messageComponent/list.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import { List, Li } from 'flowbite-svelte';
+  import * as marked from 'marked';
   
   export let ordered: boolean = false;
   export let items: Array<any>;
 </script>
 <List list={ordered ? 'decimal' : 'disc'} color="white">
   {#each items as item}
-    <Li>{item.text}</Li>
+    <Li>{@html marked
+      .parse(item.text).toString().replaceAll(/<\/?p>/g, '')}</Li>
   {/each}
 </List>
