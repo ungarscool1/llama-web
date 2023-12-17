34583fc6827c424a42341d752768e9a204579740
Léo Godard <leo@legodard.fr>
2023-12-17 14:54:16 +0100

    feat(api): add plugins management

--- api/src/models/plugin.ts
+++ api/src/models/plugin.ts
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
 
 export default new mongoose.Schema({
-  name: { type: String, required: true },
-  definition: { type: String, required: true },
+  metadata: { type: Object, required: true },
+  pipeline: { type: Object, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
   parameters: { type: Object, required: false },
+  configuration: { type: Object, require: false }
 });
---
+++ api/src/routes/plugins/index.ts
@@ -0,0 +1,109 @@
+import e, { Router } from 'express';
+import mongoose from 'mongoose';
+import * as yup from 'yup';
+
+var router = Router();
+
+router.get('/', async (req, res) => {
+  const plugins = await mongoose.model('Plugins').find();
+  plugins.sort((a, b) => {
+    if (a.createdAt > b.createdAt) {
+      return 1;
+    }
+    if (a.createdAt < b.createdAt) {
+      return -1;
+    }
+    return 0;
+  });
+  res.json(plugins.map((plugin) => ({
+    id: plugin._id,
+    name: plugin.name,
+    createdAt: plugin.createdAt
+  })));
+});
+
+router.get('/:id', async (req, res) => {
+  const plugin = await mongoose.model('Plugins').findById(req.params.id);
+  if (!plugin) {
+    return res.status(404).json({ message: 'Model not found' });
+  }
+  res.json({
+    id: plugin._id,
+    name: plugin.name,
+    definition: plugin.definition,
+    parameters: plugin.parameters,
+    createdAt: plugin.createdAt
+  });
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const plugin = await mongoose.model('Plugins').findById(req.params.id);
+    if (!plugin) {
+      return res.status(404).json({ message: 'Plugin not found' });
+    }
+    await plugin.remove();
+  } catch (e) {
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+  res.status(200).json({ message: 'Plugin deleted' });
+});
+
+router.post('/', async (req, res) => {
+  const schema = yup.object().shape({
+    metadata: yup.object().shape({
+      name: yup.string().required(),
+      description: yup.string().required(),
+      version: yup.string().required(),
+      author: yup.string().required(),
+    }).required(),
+    parameters: yup.object().required(),
+    configuration: yup.object().required(),
+    pipeline: yup.array().of(yup.object().shape({
+      id: yup.string().required(),
+      type: yup.string().required(),
+      input: yup.string().optional(),
+      output: yup.string().optional(),
+      script: yup.string().optional(),
+    })).required(),
+  })
+  let payload: yup.InferType<typeof schema>;
+  try {
+    payload = schema.validateSync(req.body)
+  } catch (e) {
+    return res.status(400).json({ message: 'Missing required fields' });
+  }
+  // check 
+  const PluginObj = mongoose.model('Plugins');
+  const plugin = new PluginObj({
+    metadata: payload.metadata,
+    pipeline: payload.pipeline,
+    parameters: payload.parameters,
+    configuration: payload.configuration
+  });
+  plugin.save();
+  res.status(201).json({ message: `Plugin ${payload.metadata.name} created` });
+});
+
+router.patch('/:id', async (req, res) => {
+  const schema = yup.object().shape({
+    promptTemplate: yup.string().required()
+  })
+  let payload: yup.InferType<typeof schema>
+
+  try {
+    payload = schema.validateSync(req.body);
+  } catch (e) {
+    console.error(e, req.body)
+    return res.status(400).json({ message: 'Bad request' });
+  }
+  try {
+    await mongoose.model('Models').findOneAndUpdate({ _id: req.params.id }, { chatPromptTemplate: payload.promptTemplate });
+  } catch (e) {
+    console.error(e)
+    return res.status(500).json({ message: 'Internal Server Error' })
+  }
+  res.status(200).json({ message: 'Model updated' });
+})
+
+export default router;
