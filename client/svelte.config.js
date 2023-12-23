970d5d0bae35b7f9c7f99a110a587bf84971d60c
Léo Godard <leo@legodard.fr>
2023-12-24 00:49:08 +0100

    fix(client): fix compilation

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,13 +1,13 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [
-		vitePreprocess(),
+		sveltekit(),
 		preprocess({
 			postcss: true
 		})
