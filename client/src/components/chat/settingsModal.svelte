fb7438082ed42d1b66f990e888e71f8646c7e5db
Léo Godard <leo@legodard.fr>
2024-02-26 23:56:28 +0100

    remove useless variables onMount function userInfo env

--- client/src/components/chat/settingsModal.svelte
+++ client/src/components/chat/settingsModal.svelte
@@ -1,22 +1,8 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { env } from '$env/dynamic/public';
-  import { Modal, Label, Select, type SelectOptionType } from 'flowbite-svelte';
+  import { Modal } from 'flowbite-svelte';
     import ChangeSystemPrompt from './settingsComponent/changeSystemPrompt.svelte';
 
   export let modalShow: boolean = false;
-  let userInfo = {
-    authenticated: false,
-    token: null
-  };
-
-  onMount(() => {
-    if (localStorage.getItem('userInfo')) {
-      userInfo = JSON.parse(localStorage.getItem('userInfo') || '{}');
-    }
-  });
-  
-  
   
   /**
    * active: text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none
