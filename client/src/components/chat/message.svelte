af3fe6b5ffa335473cd44c0a99b62e40a26d6ecb
Léo Godard <leo@legodard.fr>
2024-05-18 00:15:21 +0200

    fix(client/chat): fix xss in chat (#51)
    * fix(client/chat): fix xss in chat
    
    * test if the file is here
    
    * make: update pnpm

--- client/src/components/chat/message.svelte
+++ client/src/components/chat/message.svelte
@@ -5,12 +5,24 @@
   import Avatar from './avatar.svelte';
   import List from './messageComponent/list.svelte';
 
+  function sanitize(text: string) {
+		return text
+			.replace(/<\|[a-z]*$/, "")
+			.replace(/<\|[a-z]+\|$/, "")
+			.replace(/<$/, "")
+			.replaceAll(/<\|[a-z]+\|>/g, " ")
+			.replaceAll(/<br\s?\/?>/gi, "\n")
+			.replaceAll("<", "&lt;");
+	}
+	function unsanitize(text: string) {
+		return text.replaceAll("&lt;", "<");
+	}
   export let username: string;
   export let message: {
     message: string;
     role: string;
   };
-  $: tokens = marked.lexer(message.message);
+  $: tokens = marked.lexer(sanitize(message.message));
 </script>
 
 <div class="flex mb-2 flex-row w-full">
@@ -24,7 +36,7 @@
   <p class="flex-1 mb-0 ml-1 dark:text-white text-black whitespace-pre-line w-3/4">
     {#each tokens as token}
       {#if token.type === "code"}
-        <Codeblock code={token.text} language={token.lang} />
+        <Codeblock code={unsanitize(token.text)} language={token.lang} />
       {:else if token.type === "table"}
         <Table rows={token.rows} header={token.header} />
       {:else if token.type === "list"}
