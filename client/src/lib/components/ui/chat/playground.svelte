f4b317c430ef0901a3b20e190271f075d4c8f012
Léo Godard <leo@legodard.fr>
2024-05-30 23:12:26 +0200

    fix(ui/playground/chat): fix margin in chat bubble

--- client/src/lib/components/ui/chat/playground.svelte
+++ client/src/lib/components/ui/chat/playground.svelte
@@ -7,7 +7,7 @@
 {#if role === 'user'}
 <div class="w-full">
   <div class="py-2 px-3 text-base md:px-4 m-auto md:px-5 lg:px-1 xl:px-5">
-    <div class="mx-auto flex flex-1 gap-3 text-base md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem]">
+    <div class="ml-auto flex flex-1 gap-3 text-base md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem]">
       <div class="relative flex w-full min-w-0 flex-col">
         <div class="flex-col gap-1 md:gap-3">
           <div class="flex flex-grow flex-col max-w-full">
@@ -30,7 +30,7 @@
 {:else}
 <div class="w-full">
   <div class="py-2 px-3 text-base md:px-4 m-auto md:px-5 lg:px-1 xl:px-5">
-    <div class="mx-auto flex flex-1 gap-3 text-base md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem]">
+    <div class="mr-auto flex flex-1 gap-3 text-base md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem]">
       <div class="relative flex w-full min-w-0 flex-col">
         <div class="flex-col gap-1 md:gap-3">
           <div class="flex flex-grow flex-col max-w-full">
