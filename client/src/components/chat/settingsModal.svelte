a83ed66376c2d421b05e252acc1ed4a23e02a089
Léo Godard <leo@legodard.fr>
2024-06-02 21:02:37 +0200

    feat(ui/chat/settings): rework settings modal ui

--- client/src/components/chat/settingsModal.svelte
+++ client/src/components/chat/settingsModal.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Modal } from 'flowbite-svelte';
+  import * as Dialog from "$lib/components/ui/dialog";
   import ChangeSystemPrompt from './settingsComponent/changeSystemPrompt.svelte';
 
   export let modalShow: boolean = false;
@@ -10,13 +10,18 @@
   */
 </script>
 
-<Modal bind:open={modalShow} size="lg" autoclose={false} title="Settings" class="w-full">
-  <div class="flex flex-row gap-6">
-    <div class="flex flex-col gap-2">
-      <button class="text-black dark:text-white bg-gray-200 hover:bg-gray-700 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none">Adjust LLM prompt</button>
+<Dialog.Root bind:open={modalShow} >
+  <Dialog.Content class="md:max-w-[680px]">
+    <Dialog.Header>
+      <Dialog.Title>Settings</Dialog.Title>
+    </Dialog.Header>
+    <div class="flex flex-row gap-6">
+      <div class="flex flex-col gap-2">
+        <button class="text-black dark:text-white bg-gray-200 hover:bg-gray-700 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none">Adjust LLM prompt</button>
+      </div>
+      <div class="flex-1 flex-col">
+        <ChangeSystemPrompt />
+      </div>
     </div>
-    <div class="flex-1 flex-col">
-      <ChangeSystemPrompt />
-    </div>
-  </div>
-</Modal>
+  </Dialog.Content>
+</Dialog.Root>
