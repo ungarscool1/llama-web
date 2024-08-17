0dafd3a3a6fee0227b2a1ca64d5607e7ffc60f53
Léo Godard <leo@legodard.fr>
2024-08-17 16:35:11 +0200

    remove unused package

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,9 +5,6 @@ const config = {
 	darkMode: ["class"],
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	safelist: ["dark"],
-	plugins: [
-    require('@tailwindcss/typography'),
-  ],
 	theme: {
 		container: {
 			center: true,
