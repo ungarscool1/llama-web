217f7f4c03dbdb3fff15938d29f570e51f11eb45
Léo Godard <leo@legodard.fr>
2025-08-16 03:16:52 +0200

    chore(client): update keycloak dependencies (#86)
    * fix(client): add an error message when auth fail
    
    * chore(client): update keycloak dependencies

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -5,6 +5,7 @@
   import { goto } from '$app/navigation';
   import Icon from '$lib/components/ui/icon/icon.svelte';
 
+  let errorMessage: string = '';
   let keycloak: Keycloak;
   onMount(() => {
     if (env.PUBLIC_SKIP_AUTH === 'true') {
@@ -14,30 +15,42 @@
     keycloak = new Keycloak({
       url: env.PUBLIC_SSO_SERVER,
       realm: env.PUBLIC_SSO_REALM,
-      clientId: env.PUBLIC_CLIENT_ID,
+      clientId: env.PUBLIC_CLIENT_ID
     });
-    keycloak.init({ onLoad: 'login-required' }).then((authenticated: boolean) => {
-      if (authenticated) {
-        localStorage.setItem('userInfo', JSON.stringify({ authenticated, token: keycloak.token }));
-        const previousPage = localStorage.getItem('previousPage');
-        if (previousPage) {
-          localStorage.removeItem('previousPage');
-          goto(previousPage);
-        } else
-          goto('/playground');
-      } else {
-        // Display error message
-      }
-    });
+    if (errorMessage.length !== 0) return;
+    keycloak
+      .init({ onLoad: 'login-required' })
+      .then((authenticated: boolean) => {
+        if (authenticated) {
+          localStorage.setItem(
+            'userInfo',
+            JSON.stringify({ authenticated, token: keycloak.token })
+          );
+          const previousPage = localStorage.getItem('previousPage');
+          if (previousPage) {
+            localStorage.removeItem('previousPage');
+            goto(previousPage);
+          } else goto('/playground');
+        } else {
+          errorMessage = 'Cannot authenticate the user';
+        }
+      })
+      .catch((err) => {
+        errorMessage = `Error during Keycloak initialization: ${err}`;
+      });
   });
 </script>
 
 <div class="grid h-screen place-items-center dark:text-white">
   <h1 class="text-4xl font-bold">Welcome to llama web</h1>
   <div class="flex flex-col items-center">
-    {#if env.PUBLIC_SKIP_AUTH === 'false' || !env.PUBLIC_SKIP_AUTH}
-      <p>Login in progress</p>
+    {#if errorMessage.length === 0}
+      {#if env.PUBLIC_SKIP_AUTH === 'false' || !env.PUBLIC_SKIP_AUTH}
+        <p>Login in progress</p>
+      {/if}
+      <Icon name="loader-circle" class="animate-spin" />
+    {:else}
+      <p>{errorMessage}</p>
     {/if}
-    <Icon name="loader-circle" class="animate-spin" />
   </div>
 </div>
